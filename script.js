1a239b1961c6a92a50220ae81a0227862c11ee6a
NiJeTi <nijeti.inc@gmail.com>
2019-02-23 22:22:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿    var url = window.location.href;
+    if(url.indexOf("disable_polymer=true") == -1)
+    {
+        separator = (url.indexOf("?") === -1) ? "?": "&";
+        newParam = separator + "disable_polymer=true";
+        newUrl = url.replace(newParam, "");
+        newUrl += newParam;
+        window.location.href = newUrl;
+    }
