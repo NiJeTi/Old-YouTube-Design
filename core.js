b0c21a1bcc245660d9ff284a1deeb85585c383f3
NiJeTi <nijeti.inc@gmail.com>
2020-05-23 01:58:22 +0300

    Refactoring core logic

--- core.js
+++ core.js
@@ -1,9 +1,7 @@
-﻿    var url = window.location.href;
-    if(url.indexOf("disable_polymer=true") == -1)
-    {
-        separator = (url.indexOf("?") === -1) ? "?": "&";
-        newParam = separator + "disable_polymer=true";
-        newUrl = url.replace(newParam, "");
-        newUrl += newParam;
-        window.location.href = newUrl;
-    }
+﻿const url = window.location.href;
+
+if (url.indexOf('disable_polymer=true') === -1) {
+    const urlParams = ((url.indexOf('?') === -1) ? '?' : '&') + 'disable_polymer=true';
+
+    window.location.href = url.replace(urlParams, '') + urlParams;
+}
